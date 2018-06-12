efd3fd1176c6746bb6c6e99ead7a1ef2b9f5c7d3
Nahuel <nahuelcabrerawork@gmail.com>
2018-06-12 07:02:12 -0300

    finished rest api

---
+++ http_req_n_rest_api/README.md
@@ -0,0 +1,8 @@
+# http_req_n_rest_api
+
+A new Flutter application.
+
+## Getting Started
+
+For help getting started with Flutter, view our online
+[documentation](https://flutter.io/).
---
+++ http_req_n_rest_api/lib/main.dart
@@ -0,0 +1,49 @@
+import 'dart:async';
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+void main()
+{
+  runApp(MaterialApp(
+    home: HomePage()
+  ));
+}
+
+class HomePage extends StatefulWidget
+{
+  @override
+  HomePageState createState() => HomePageState();
+}
+
+class HomePageState extends State<HomePage>
+{
+
+  Future<String> getData() async
+  {
+    http.Response response = await http.get(
+      Uri.encodeFull("https://jsonplaceholder.typicode.com/posts"),
+      headers:
+      {
+        "Accept": "application/json"
+      }
+    );
+
+    print(response.body);
+
+    List data = JSON.decode(response.body);
+    print(data[1]["title"]);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: RaisedButton(
+          child: Text("Get data, see console"),
+          onPressed: getData,
+        ),
+      ),
+    );
+  }
+}
